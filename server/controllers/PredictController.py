40fc971cf09c232b6075b8065f3bbdfcd534edfb
Nguyenle23___ <beenbin2308@gmail.com>
2023-08-17 15:44:45 +0700

    update predict CO2, UV, PM2.5, Humidity for server

--- server/controllers/PredictController.py
+++ server/controllers/PredictController.py
@@ -5,6 +5,126 @@ from joblib import load
 import os
 
 class PredictController:
+  def predictTemp():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataTemp']
+      originalArray = np.array(tempDate)
+
+      # #reshape data to (1, 12, 1)
+      window_size = 12
+      data_batches = originalArray[:window_size].reshape(1, 12, 1)
+
+      script_dir = os.path.dirname(os.path.abspath(__file__))
+      server_dir = os.path.dirname(os.path.dirname(script_dir))
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/model_pm25.h5')
+      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/model_pm25.json')
+      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/scalerPM25.joblib')
+
+      if os.path.exists(model_path) and os.path.exists(model_json_path):
+        json_file = open(model_json_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        loaded_model = model_from_json(loaded_model_json)
+
+        loaded_model.load_weights(model_path)
+
+        loaded_model.compile(optimizer='adam', loss='mse')
+
+        prediction = loaded_model.predict(data_batches)
+
+        #scale back the data to the original representation
+        scaler = load(scaler_path)
+        prediction = scaler.inverse_transform(prediction)
+        print(prediction[0])
+
+      else:
+        print(f"File not found: {model_path}")
+      
+    return jsonify(prediction[0].tolist())
+  
+  def predictHumi():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataHumi']
+      originalArray = np.array(tempDate)
+
+      # #reshape data to (1, 12, 1)
+      window_size = 12
+      data_batches = originalArray[:window_size].reshape(1, 12, 1)
+
+      script_dir = os.path.dirname(os.path.abspath(__file__))
+      server_dir = os.path.dirname(os.path.dirname(script_dir))
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/model_pm25.h5')
+      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/model_pm25.json')
+      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/scalerPM25.joblib')
+
+      if os.path.exists(model_path) and os.path.exists(model_json_path):
+        json_file = open(model_json_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        loaded_model = model_from_json(loaded_model_json)
+
+        loaded_model.load_weights(model_path)
+
+        loaded_model.compile(optimizer='adam', loss='mse')
+
+        prediction = loaded_model.predict(data_batches)
+
+        #scale back the data to the original representation
+        scaler = load(scaler_path)
+        prediction = scaler.inverse_transform(prediction)
+        print(prediction[0])
+
+      else:
+        print(f"File not found: {model_path}")
+      
+    return jsonify(prediction[0].tolist())
+  
+  def predictCO2():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataCO2']
+      originalArray = np.array(tempDate)
+
+      # #reshape data to (1, 12, 1)
+      window_size = 12
+      data_batches = originalArray[:window_size].reshape(1, 12, 1)
+
+      script_dir = os.path.dirname(os.path.abspath(__file__))
+      server_dir = os.path.dirname(os.path.dirname(script_dir))
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/predictCO2/model_co2.h5')
+      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictCO2/model_co2.json')
+      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictCO2/scalerCO2.joblib')
+
+      if os.path.exists(model_path) and os.path.exists(model_json_path):
+        json_file = open(model_json_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        loaded_model = model_from_json(loaded_model_json)
+
+        loaded_model.load_weights(model_path)
+
+        loaded_model.compile(optimizer='adam', loss='mse')
+
+        prediction = loaded_model.predict(data_batches)
+
+        #scale back the data to the original representation
+        scaler = load(scaler_path)
+        prediction = scaler.inverse_transform(prediction)
+        print(prediction[0])
+
+      else:
+        print(f"File not found: {model_path}")
+      
+    return jsonify(prediction[0].tolist())
+  
   def predictCO():
     if request.method == 'POST':
       data = request.json
@@ -18,9 +138,89 @@ class PredictController:
       script_dir = os.path.dirname(os.path.abspath(__file__))
       server_dir = os.path.dirname(os.path.dirname(script_dir))
 
-      model_path = os.path.join(server_dir, 'server/datasets/models/model_co.h5')
-      model_json_path = os.path.join(server_dir, 'server/datasets/models/model_co.json')
-      scaler_path = os.path.join(server_dir, 'server/datasets/models/scalerCO.joblib')
+      model_path = os.path.join(server_dir, 'server/datasets/models/predictCO/model_co.h5')
+      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictCO/model_co.json')
+      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictCO/scalerCO.joblib')
+
+      if os.path.exists(model_path) and os.path.exists(model_json_path):
+        json_file = open(model_json_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        loaded_model = model_from_json(loaded_model_json)
+
+        loaded_model.load_weights(model_path)
+
+        loaded_model.compile(optimizer='adam', loss='mse')
+
+        prediction = loaded_model.predict(data_batches)
+
+        #scale back the data to the original representation
+        scaler = load(scaler_path)
+        prediction = scaler.inverse_transform(prediction)
+        print(prediction[0])
+
+      else:
+        print(f"File not found: {model_path}")
+      
+    return jsonify(prediction[0].tolist())
+  
+  def predictUV():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataUV']
+      originalArray = np.array(tempDate)
+
+      # #reshape data to (1, 12, 1)
+      window_size = 12
+      data_batches = originalArray[:window_size].reshape(1, 12, 1)
+
+      script_dir = os.path.dirname(os.path.abspath(__file__))
+      server_dir = os.path.dirname(os.path.dirname(script_dir))
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/predictUV/model_uv.h5')
+      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictUV/model_uv.json')
+      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictUV/scalerUV.joblib')
+
+      if os.path.exists(model_path) and os.path.exists(model_json_path):
+        json_file = open(model_json_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        loaded_model = model_from_json(loaded_model_json)
+
+        loaded_model.load_weights(model_path)
+
+        loaded_model.compile(optimizer='adam', loss='mse')
+
+        prediction = loaded_model.predict(data_batches)
+
+        #scale back the data to the original representation
+        scaler = load(scaler_path)
+        prediction = scaler.inverse_transform(prediction)
+        print(prediction[0])
+
+      else:
+        print(f"File not found: {model_path}")
+      
+    return jsonify(prediction[0].tolist())
+  
+  def predictPM25():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataPM25']
+      originalArray = np.array(tempDate)
+
+      # #reshape data to (1, 12, 1)
+      window_size = 12
+      data_batches = originalArray[:window_size].reshape(1, 12, 1)
+
+      script_dir = os.path.dirname(os.path.abspath(__file__))
+      server_dir = os.path.dirname(os.path.dirname(script_dir))
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/model_pm25.h5')
+      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/model_pm25.json')
+      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictPM25/scalerPM25.joblib')
 
       if os.path.exists(model_path) and os.path.exists(model_json_path):
         json_file = open(model_json_path, 'r')
--- server/routes/Predict.py
+++ server/routes/Predict.py
@@ -4,5 +4,10 @@ from controllers.PredictController import PredictController
 
 Predict = Blueprint('Predict', __name__)
 
+Predict.route('/temp', methods = ['POST'])(PredictController.predictTemp)
+Predict.route('/humi', methods = ['POST'])(PredictController.predictHumi)
 Predict.route('/co', methods = ['POST'])(PredictController.predictCO)
+Predict.route('/co2', methods = ['POST'])(PredictController.predictCO2)
+Predict.route('/uv', methods = ['POST'])(PredictController.predictUV)
+Predict.route('/pm25', methods = ['POST'])(PredictController.predictPM25)
 
