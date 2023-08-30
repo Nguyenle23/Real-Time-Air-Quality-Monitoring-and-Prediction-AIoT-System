8130a571903d2ee2cea73b236400e033af30c855
Nguyenle23___ <beenbin2308@gmail.com>
2023-08-30 13:33:34 +0700

    update server feature

--- server/controllers/PredictController.py
+++ server/controllers/PredictController.py
@@ -3,46 +3,165 @@ import numpy as np
 from tensorflow.keras.models import load_model, model_from_json
 from joblib import load
 import os
+import pandas as pd
+from datetime import datetime
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+server_dir = os.path.dirname(os.path.dirname(script_dir))
 
 class PredictController:
-  def predictTemp():
+  def predictLRTemp():
     if request.method == 'POST':
       data = request.json
       tempDate = data['dataTemp']
       originalArray = np.array(tempDate)
+      input_datetime_str = pd.to_datetime(originalArray)
+      input_datetime_str = str(input_datetime_str.max())
 
-      # #reshape data to (1, 12, 1)
-      window_size = 12
-      data_batches = originalArray[:window_size].reshape(1, 12, 1)
+      # Parse the input datetime string
+      input_datetime = datetime.strptime(input_datetime_str, "%Y-%m-%d %H:%M:%S%z")
 
-      script_dir = os.path.dirname(os.path.abspath(__file__))
-      server_dir = os.path.dirname(os.path.dirname(script_dir))
+      # Format the datetime as desired
+      formatted_datetime = input_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-      model_path = os.path.join(server_dir, 'server/datasets/models/predictTemp/model_temp.h5')
-      model_json_path = os.path.join(server_dir, 'server/datasets/models/predictTemp/model_temp.json')
-      scaler_path = os.path.join(server_dir, 'server/datasets/models/predictTemp/scalerTemp.joblib')
+      old_date = pd.to_datetime(formatted_datetime)
 
-      if os.path.exists(model_path) and os.path.exists(model_json_path):
-        json_file = open(model_json_path, 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
+      # #get current date
+      current_date = pd.Timestamp.now()
 
-        loaded_model = model_from_json(loaded_model_json)
+      time_differences = (current_date - old_date).total_seconds()
 
-        loaded_model.load_weights(model_path)
-
-        loaded_model.compile(optimizer='adam', loss='mse')
-
-        prediction = loaded_model.predict(data_batches)
-
-        #scale back the data to the original representation
-        scaler = load(scaler_path)
-        prediction = scaler.inverse_transform(prediction)
+      model_path = os.path.join(server_dir, 'server/datasets/models/linear_regression/model_lr_temp.pkl')
+      if os.path.exists(model_path):
+        loaded_model = load(model_path)
+        prediction = loaded_model.predict([[time_differences]])
         print(prediction[0])
-
       else:
         print(f"File not found: {model_path}")
-      
+    return jsonify(prediction[0].tolist())
+
+  def predictGBTemp():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataTemp']
+      originalArray = np.array(tempDate)
+      input_datetime_str = pd.to_datetime(originalArray)
+      input_datetime_str = str(input_datetime_str.max())
+
+      # Parse the input datetime string
+      input_datetime = datetime.strptime(input_datetime_str, "%Y-%m-%d %H:%M:%S%z")
+
+      # Format the datetime as desired
+      formatted_datetime = input_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+      old_date = pd.to_datetime(formatted_datetime)
+
+      # #get current date
+      current_date = pd.Timestamp.now()
+
+      time_differences = (current_date - old_date).total_seconds()
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/gradient_boost/model_gb_temp.pkl')
+      if os.path.exists(model_path):
+        loaded_model = load(model_path)
+        prediction = loaded_model.predict([[time_differences]])
+        print(prediction[0])
+      else:
+        print(f"File not found: {model_path}")
+    return jsonify(prediction[0].tolist())
+
+  def predictXGBTemp():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataTemp']
+      originalArray = np.array(tempDate)
+      input_datetime_str = pd.to_datetime(originalArray)
+      input_datetime_str = str(input_datetime_str.max())
+
+      # Parse the input datetime string
+      input_datetime = datetime.strptime(input_datetime_str, "%Y-%m-%d %H:%M:%S%z")
+
+      # Format the datetime as desired
+      formatted_datetime = input_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+      old_date = pd.to_datetime(formatted_datetime)
+
+      # #get current date
+      current_date = pd.Timestamp.now()
+
+      time_differences = (current_date - old_date).total_seconds()
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/gradient_boost/model_gb_temp.pkl')
+      if os.path.exists(model_path):
+        loaded_model = load(model_path)
+        prediction = loaded_model.predict([[time_differences]])
+        print(prediction[0])
+      else:
+        print(f"File not found: {model_path}")
+    return jsonify(prediction[0].tolist())
+
+  def predictRFTemp():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataTemp']
+      originalArray = np.array(tempDate)
+      input_datetime_str = pd.to_datetime(originalArray)
+      input_datetime_str = str(input_datetime_str.max())
+
+      # Parse the input datetime string
+      input_datetime = datetime.strptime(input_datetime_str, "%Y-%m-%d %H:%M:%S%z")
+
+      # Format the datetime as desired
+      formatted_datetime = input_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+      old_date = pd.to_datetime(formatted_datetime)
+
+      # #get current date
+      current_date = pd.Timestamp.now()
+
+      time_differences = (current_date - old_date).total_seconds()
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/k_nearest_neighboor/model_knn_temp.pkl')
+      if os.path.exists(model_path):
+        loaded_model = load(model_path)
+        prediction = loaded_model.predict([[time_differences]])
+        print(prediction[0])
+      else:
+        print(f"File not found: {model_path}")
+    return jsonify(prediction[0].tolist())
+
+  def predictKNNTemp():
+    if request.method == 'POST':
+      data = request.json
+      tempDate = data['dataTemp']
+      originalArray = np.array(tempDate)
+      input_datetime_str = pd.to_datetime(originalArray)
+      input_datetime_str = str(input_datetime_str.max())
+
+      # Parse the input datetime string
+      input_datetime = datetime.strptime(input_datetime_str, "%Y-%m-%d %H:%M:%S%z")
+
+      # Format the datetime as desired
+      formatted_datetime = input_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+      old_date = pd.to_datetime(formatted_datetime)
+
+      # #get current date
+      current_date = pd.Timestamp.now()
+
+      time_differences = (current_date - old_date).total_seconds()
+
+      model_path = os.path.join(server_dir, 'server/datasets/models/random_forest/model_rf_temp.pkl')
+      if os.path.exists(model_path):
+        loaded_model = load(model_path)
+        prediction = loaded_model.predict([[time_differences]])
+        print(prediction[0])
+      else:
+        print(f"File not found: {model_path}")
     return jsonify(prediction[0].tolist())
   
   def predictHumi():
--- server/routes/Predict.py
+++ server/routes/Predict.py
@@ -4,7 +4,12 @@ from controllers.PredictController import PredictController
 
 Predict = Blueprint('Predict', __name__)
 
-Predict.route('/temp', methods = ['POST'])(PredictController.predictTemp)
+Predict.route('/lr/temp', methods = ['POST'])(PredictController.predictLRTemp)
+Predict.route('/gb/temp', methods = ['POST'])(PredictController.predictGBTemp)
+Predict.route('/xgb/temp', methods = ['POST'])(PredictController.predictXGBTemp)
+Predict.route('/rf/temp', methods = ['POST'])(PredictController.predictRFTemp)
+Predict.route('/knn/temp', methods = ['POST'])(PredictController.predictKNNTemp)
+
 Predict.route('/humi', methods = ['POST'])(PredictController.predictHumi)
 Predict.route('/co', methods = ['POST'])(PredictController.predictCO)
 Predict.route('/co2', methods = ['POST'])(PredictController.predictCO2)
