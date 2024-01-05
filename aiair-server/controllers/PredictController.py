bceca5c3926bb465427c534170c84ea8c61c6354
Nguyenle23___ <beenbin2308@gmail.com>
2024-01-05 11:20:28 +0700

    update LSTM feature for remain factors in production

--- aiair-server/controllers/PredictController.py
+++ aiair-server/controllers/PredictController.py
@@ -7,6 +7,9 @@ from prophet import Prophet
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import model_from_json
 
+import warnings
+warnings.filterwarnings('ignore')
+
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 
@@ -76,6 +79,216 @@ class PredictController:
       
     return jsonify(objectFormat)
   
+  def predictHumiProphet():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataHumi']
+
+        humiTime = []
+        for i in objectFormat['time']:
+          humiTime.append(i)
+
+        humiData = []
+        for i in objectFormat['value']:
+          humiData.append(i)
+
+        arrayData = np.array(humiData)
+        arrayTime = np.array(humiTime)
+        datetimeHumi = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeHumi, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        model_prophet = Prophet()
+        model_prophet.fit(dataset)
+
+        future = model_prophet.make_future_dataframe(periods=12, freq='5T')
+        forecast = model_prophet.predict(future)
+        forecast = forecast.tail(12)
+
+        forecast = forecast[['ds', 'yhat']]
+        forecast = forecast.set_index('ds')
+        forecast.reset_index(inplace=True)
+
+        arrayForecast = np.array(forecast['yhat'])
+        arrayForecast = np.around(arrayForecast, decimals=2)
+        listForecast = arrayForecast.tolist()
+        objectFormat['forecast'] = listForecast
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictCO2Prophet():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataCO2']
+
+        co2Time = []
+        for i in objectFormat['time']:
+          co2Time.append(i)
+
+        co2Data = []
+        for i in objectFormat['value']:
+          co2Data.append(i)
+
+        arrayData = np.array(co2Data)
+        arrayTime = np.array(co2Time)
+        datetimeCO2 = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeCO2, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        model_prophet = Prophet()
+        model_prophet.fit(dataset)
+
+        future = model_prophet.make_future_dataframe(periods=12, freq='5T')
+        forecast = model_prophet.predict(future)
+        forecast = forecast.tail(12)
+
+        forecast = forecast[['ds', 'yhat']]
+        forecast = forecast.set_index('ds')
+        forecast.reset_index(inplace=True)
+
+        arrayForecast = np.array(forecast['yhat'])
+        arrayForecast = np.around(arrayForecast, decimals=2)
+        listForecast = arrayForecast.tolist()
+        objectFormat['forecast'] = listForecast
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictCOProphet():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataCO']
+
+        coTime = []
+        for i in objectFormat['time']:
+          coTime.append(i)
+
+        coData = []
+        for i in objectFormat['value']:
+          coData.append(i)
+
+        arrayData = np.array(coData)
+        arrayTime = np.array(coTime)
+        datetimeCO = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeCO, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        model_prophet = Prophet()
+        model_prophet.fit(dataset)
+
+        future = model_prophet.make_future_dataframe(periods=12, freq='5T')
+        forecast = model_prophet.predict(future)
+        forecast = forecast.tail(12)
+
+        forecast = forecast[['ds', 'yhat']]
+        forecast = forecast.set_index('ds')
+        forecast.reset_index(inplace=True)
+
+        arrayForecast = np.array(forecast['yhat'])
+        arrayForecast = np.around(arrayForecast, decimals=2)
+        listForecast = arrayForecast.tolist()
+        objectFormat['forecast'] = listForecast
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictUVProphet():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataUV']
+
+        uvTime = []
+        for i in objectFormat['time']:
+          uvTime.append(i)
+
+        uvData = []
+        for i in objectFormat['value']:
+          uvData.append(i)
+
+        arrayData = np.array(uvData)
+        arrayTime = np.array(uvTime)
+        datetimeUV = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeUV, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        model_prophet = Prophet()
+        model_prophet.fit(dataset)
+
+        future = model_prophet.make_future_dataframe(periods=12, freq='5T')
+        forecast = model_prophet.predict(future)
+        forecast = forecast.tail(12)
+
+        forecast = forecast[['ds', 'yhat']]
+        forecast = forecast.set_index('ds')
+        forecast.reset_index(inplace=True)
+
+        arrayForecast = np.array(forecast['yhat'])
+        arrayForecast = np.around(arrayForecast, decimals=2)
+        listForecast = arrayForecast.tolist()
+        objectFormat['forecast'] = listForecast
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictPM25Prophet():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataPM25']
+
+        pm25Time = []
+        for i in objectFormat['time']:
+          pm25Time.append(i)
+
+        pm25Data = []
+        for i in objectFormat['value']:
+          pm25Data.append(i)
+
+        arrayData = np.array(pm25Data)
+        arrayTime = np.array(pm25Time)
+        datetimePM25 = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimePM25, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        model_prophet = Prophet()
+        model_prophet.fit(dataset)
+
+        future = model_prophet.make_future_dataframe(periods=12, freq='5T')
+        forecast = model_prophet.predict(future)
+        forecast = forecast.tail(12)
+
+        forecast = forecast[['ds', 'yhat']]
+        forecast = forecast.set_index('ds')
+        forecast.reset_index(inplace=True)
+
+        arrayForecast = np.array(forecast['yhat'])
+        arrayForecast = np.around(arrayForecast, decimals=2)
+        listForecast = arrayForecast.tolist()
+        objectFormat['forecast'] = listForecast
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
   #-------------------LSTM-------------------
   def predictTempLSTM():
     if request.method == 'POST':
@@ -99,6 +312,9 @@ class PredictController:
         dataset = pd.DataFrame({'ds': datetimeTemp, 'y': arrayData})
         dataset = dataset.set_index('ds')
         dataset = dataset.resample('5T').ffill()
+        dataset = dataset.dropna()
+        dataset = dataset.iloc[1:]
+        
         dataset.reset_index(inplace=True)
 
         # Scale the data to be between 0 and 1
@@ -118,8 +334,8 @@ class PredictController:
         input_data = padded_temp.reshape((1, 1, sequence_length))
         
         # Load model architecture from JSON file
-        temp_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/temp-lstm.json')
-        temp_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/temp_lstm_weight.h5')
+        temp_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/temp_pc_lstm_weight.json')
+        temp_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/temp_pc_lstm_weight.h5')
         with open(temp_lstm_json, 'r') as json_file:
             loaded_model_json = json_file.read()
         
@@ -154,3 +370,281 @@ class PredictController:
         print(e)
       
     return jsonify(objectFormat)
+
+  def predictHumiLSTM():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataHumi']
+
+        # push data to array
+        humiTime = []
+        for i in objectFormat['time']:
+          humiTime.append(i)
+
+        humiData = []
+        for i in objectFormat['value']:
+          humiData.append(i)
+
+        arrayData = np.array(humiData)
+        arrayTime = np.array(humiTime)
+        datetimeHumi = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeHumi, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset = dataset.dropna()
+        dataset = dataset.iloc[1:]
+        dataset.reset_index(inplace=True)
+
+        scaler = MinMaxScaler()
+        scaled_humi = scaler.fit_transform(dataset[['y']])
+
+        sequence_length = 100
+        if len(scaled_humi) < sequence_length:
+            padded_humi = np.pad(scaled_humi, ((sequence_length - len(scaled_humi), 0), (0, 0)), mode='constant')
+        else:
+            padded_humi = scaled_humi[-sequence_length:]
+        input_data = padded_humi.reshape((1, 1, sequence_length))
+        
+        humi_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/humi-lstm.json')
+        humi_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/humi_lstm_weight.h5')
+        with open(humi_lstm_json, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights(humi_lstm_weight)
+
+        if os.path.exists(humi_lstm_weight):
+          predictions = loaded_model.predict(input_data)
+          predictions_inv = scaler.inverse_transform(predictions)[0]
+          arrayForecast = np.array(predictions_inv)
+          arrayForecast = np.around(arrayForecast, decimals=4)
+          listForecast = arrayForecast.tolist()
+          objectFormat['forecast'] = listForecast
+        else:
+          print(f"File not found: {humi_lstm_weight}")
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictCO2LSTM():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataCO2']
+
+        # push data to array
+        co2Time = []
+        for i in objectFormat['time']:
+          co2Time.append(i)
+
+        co2Data = []
+        for i in objectFormat['value']:
+          co2Data.append(i)
+
+        arrayData = np.array(co2Data)
+        arrayTime = np.array(co2Time)
+        datetimeCO2 = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeCO2, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        scaler = MinMaxScaler()
+        scaled_co2 = scaler.fit_transform(dataset[['y']])
+
+        sequence_length = 100
+        if len(scaled_co2) < sequence_length:
+            padded_co2 = np.pad(scaled_co2, ((sequence_length - len(scaled_co2), 0), (0, 0)), mode='constant')
+        else:
+            padded_co2 = scaled_co2[-sequence_length:]
+        input_data = padded_co2.reshape((1, 1, sequence_length))
+        
+        co2_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/co2-lstm.json')
+        co2_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/co2_lstm_weight.h5')
+        with open(co2_lstm_json, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights(co2_lstm_weight)
+
+        if os.path.exists(co2_lstm_weight):
+          predictions = loaded_model.predict(input_data)
+          predictions_inv = scaler.inverse_transform(predictions)[0]
+          arrayForecast = np.array(predictions_inv)
+          arrayForecast = np.around(arrayForecast, decimals=4)
+          listForecast = arrayForecast.tolist()
+          objectFormat['forecast'] = listForecast
+        else:
+          print(f"File not found: {co2_lstm_weight}")
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictCOLSTM():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataCO']
+
+        # push data to array
+        coTime = []
+        for i in objectFormat['time']:
+          coTime.append(i)
+
+        coData = []
+        for i in objectFormat['value']:
+          coData.append(i)
+
+        arrayData = np.array(coData)
+        arrayTime = np.array(coTime)
+        datetimeCO = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeCO, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        scaler = MinMaxScaler()
+        scaled_co = scaler.fit_transform(dataset[['y']])
+
+        sequence_length = 100
+        if len(scaled_co) < sequence_length:
+            padded_co = np.pad(scaled_co, ((sequence_length - len(scaled_co), 0), (0, 0)), mode='constant')
+        else:
+            padded_co = scaled_co[-sequence_length:]
+        input_data = padded_co.reshape((1, 1, sequence_length))
+        
+        co_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/co-lstm.json')
+        co_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/co_lstm_weight.h5')
+        with open(co_lstm_json, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights(co_lstm_weight)
+
+        if os.path.exists(co_lstm_weight):
+          predictions = loaded_model.predict(input_data)
+          predictions_inv = scaler.inverse_transform(predictions)[0]
+          arrayForecast = np.array(predictions_inv)
+          arrayForecast = np.around(arrayForecast, decimals=4)
+          listForecast = arrayForecast.tolist()
+          objectFormat['forecast'] = listForecast
+        else:
+          print(f"File not found: {co_lstm_weight}")
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictUVLSTM():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataUV']
+
+        # push data to array
+        uvTime = []
+        for i in objectFormat['time']:
+          uvTime.append(i)
+
+        uvData = []
+        for i in objectFormat['value']:
+          uvData.append(i)
+
+        arrayData = np.array(uvData)
+        arrayTime = np.array(uvTime)
+        datetimeUV = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeUV, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        scaler = MinMaxScaler()
+        scaled_uv = scaler.fit_transform(dataset[['y']])
+
+        sequence_length = 100
+        if len(scaled_uv) < sequence_length:
+            padded_uv = np.pad(scaled_uv, ((sequence_length - len(scaled_uv), 0), (0, 0)), mode='constant')
+        else:
+            padded_uv = scaled_uv[-sequence_length:]
+        input_data = padded_uv.reshape((1, 1, sequence_length))
+        
+        uv_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/uv-lstm.json')
+        uv_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/uv_lstm_weight.h5')
+        with open(uv_lstm_json, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights(uv_lstm_weight)
+
+        if os.path.exists(uv_lstm_weight):
+          predictions = loaded_model.predict(input_data)
+          predictions_inv = scaler.inverse_transform(predictions)[0]
+          arrayForecast = np.array(predictions_inv)
+          arrayForecast = np.around(arrayForecast, decimals=4)
+          listForecast = arrayForecast.tolist()
+          objectFormat['forecast'] = listForecast
+        else:
+          print(f"File not found: {uv_lstm_weight}")
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
+  def predictPM25LSTM():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataPM25']
+
+        # push data to array
+        pm25Time = []
+        for i in objectFormat['time']:
+          pm25Time.append(i)
+
+        pm25Data = []
+        for i in objectFormat['value']:
+          pm25Data.append(i)
+
+        arrayData = np.array(pm25Data)
+        arrayTime = np.array(pm25Time)
+        datetimePM25 = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimePM25, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset.reset_index(inplace=True)
+
+        scaler = MinMaxScaler()
+        scaled_pm25 = scaler.fit_transform(dataset[['y']])
+
+        sequence_length = 100
+        if len(scaled_pm25) < sequence_length:
+            padded_pm25 = np.pad(scaled_pm25, ((sequence_length - len(scaled_pm25), 0), (0, 0)), mode='constant')
+        else:
+            padded_pm25 = scaled_pm25[-sequence_length:]
+        input_data = padded_pm25.reshape((1, 1, sequence_length))
+        
+        pm25_lstm_json = os.path.join(server_dir, 'server/datasets/models/lstm/pm25-lstm.json')
+        pm25_lstm_weight = os.path.join(server_dir, 'server/datasets/models/lstm/pm25_lstm_weight.h5')
+        with open(pm25_lstm_json, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights(pm25_lstm_weight)
+
+        if os.path.exists(pm25_lstm_weight):
+          predictions = loaded_model.predict(input_data)
+          predictions_inv = scaler.inverse_transform(predictions)[0]
+          arrayForecast = np.array(predictions_inv)
+          arrayForecast = np.around(arrayForecast, decimals=4)
+          listForecast = arrayForecast.tolist()
+          objectFormat['forecast'] = listForecast
+        else:
+          print(f"File not found: {pm25_lstm_weight}")
+      except Exception as e:
+        print(e)
+    return jsonify(objectFormat)
+  
--- aiair-server/routes/Predict.py
+++ aiair-server/routes/Predict.py
@@ -5,5 +5,16 @@ from controllers.PredictController import PredictController
 Predict = Blueprint('Predict', __name__)
 
 Predict.route('/prophet/temp', methods = ['POST'])(PredictController.predictTempProphet)
+Predict.route('/prophet/humi', methods = ['POST'])(PredictController.predictHumiProphet)
+Predict.route('/prophet/co2', methods = ['POST'])(PredictController.predictCO2Prophet)
+Predict.route('/prophet/co', methods = ['POST'])(PredictController.predictCOProphet)
+Predict.route('/prophet/uv', methods = ['POST'])(PredictController.predictUVProphet)
+Predict.route('/prophet/pm25', methods = ['POST'])(PredictController.predictPM25Prophet)
+
 Predict.route('/lstm/temp', methods = ['POST'])(PredictController.predictTempLSTM)
+Predict.route('/lstm/humi', methods = ['POST'])(PredictController.predictHumiLSTM)
+Predict.route('/lstm/co2', methods = ['POST'])(PredictController.predictCO2LSTM)
+Predict.route('/lstm/co', methods = ['POST'])(PredictController.predictCOLSTM)
+Predict.route('/lstm/uv', methods = ['POST'])(PredictController.predictUVLSTM)
+Predict.route('/lstm/pm25', methods = ['POST'])(PredictController.predictPM25LSTM)
 
