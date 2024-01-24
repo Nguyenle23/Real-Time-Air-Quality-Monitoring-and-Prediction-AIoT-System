7ec95319e16e7fd022469cedcbf03d19c4365bc9
Nguyenle23___ <beenbin2308@gmail.com>
2024-01-24 15:52:40 +0700

    add feature forecast with BPNN

--- aiair-server/controllers/PredictController.py
+++ aiair-server/controllers/PredictController.py
@@ -24,6 +24,123 @@ p_rf = {'n_estimators': 1000, 'max_depth': 10, 'random_state': RANDOM_SEED}
 p_knn = {'n_neighbors': 3}
 
 class PredictController:
+  #-------------------Prophet-LSTM-------------------
+  def predictTempProphetLSTM():
+    if request.method == 'POST':
+      try:
+        data = request.json
+        objectFormat = data['dataTemp']
+
+        # push data to array
+        tempTime = []
+        for i in objectFormat['time']:
+          tempTime.append(i)
+
+        tempData = []
+        for i in objectFormat['value']:
+          tempData.append(i)
+
+        arrayData = np.array(tempData)
+        arrayTime = np.array(tempTime)
+        datetimeTemp = pd.to_datetime(arrayTime)
+
+        dataset = pd.DataFrame({'ds': datetimeTemp, 'y': arrayData})
+        dataset = dataset.set_index('ds')
+        dataset = dataset.resample('5T').ffill()
+        dataset = dataset.dropna()
+        dataset = dataset.iloc[1:]
+        dataset.reset_index(inplace=True)
+
+        scaler = MinMaxScaler()
+        scaled_temp = scaler.fit_transform(dataset[['y']])
+        sequence_length = 12
+        if len(scaled_temp) < sequence_length:
+            padded_temp = np.pad(scaled_temp, ((sequence_length - len(scaled_temp), 0), (0, 0)), mode='constant')
+        else:
+            padded_temp = scaled_temp[-sequence_length:]
+        input_data = padded_temp.reshape((1, 1, sequence_length))
+        
+        # Load model LSTM
+        temp_lstm_json = os.path.join(server_dir, 'aiair-server/datasets/models/lstm/test-lstm.json')
+        temp_lstm_weight = os.path.join(server_dir, 'aiair-server/datasets/models/lstm/test_lstm_weight.h5')
+        with open(temp_lstm_json, 'r') as json_file:
+            loaded_model_json_lstm = json_file.read()
+        
+        loaded_model_lstm = model_from_json(loaded_model_json_lstm)
+        loaded_model_lstm.load_weights(temp_lstm_weight)
+
+        # Load model BPNN (json and h5)
+        temp_bpnn_json = os.path.join(server_dir, 'aiair-server/datasets/models/prophet-lstm/temp-bpnn-model.json')
+        temp_bpnn_weight = os.path.join(server_dir, 'aiair-server/datasets/models/prophet-lstm/temp-bpnn-model.h5')
+        with open(temp_bpnn_json, 'r') as json_file:
+            loaded_model_json_bpnn = json_file.read()
+
+        loaded_model_bpnn = model_from_json(loaded_model_json_bpnn)
+        loaded_model_bpnn.load_weights(temp_bpnn_weight)
+
+        if os.path.exists(temp_lstm_weight) and os.path.exists(temp_bpnn_weight):
+          #-----------lstm-----------
+          print("--------model loaded lstm---------")
+          predictions = loaded_model_lstm.predict(input_data)
+          predictions_inv = scaler.inverse_transform(predictions)[0]
+          arrayForecast = np.array(predictions_inv)
+          arrayForecast = np.around(arrayForecast, decimals=4)
+          lstmForecast = arrayForecast
+
+          #-----------prophet-----------
+          print("--------model loaded prophet---------")
+          dataset['ds'] = dataset['ds'].dt.tz_localize(None)
+
+          model_prophet = Prophet()
+          model_prophet.fit(dataset)
+
+          future = model_prophet.make_future_dataframe(periods=12, freq='5T')
+          prophetForecast = model_prophet.predict(future)
+          prophetForecast = prophetForecast.tail(12)
+
+          # get only ds and yhat
+          prophetForecast = prophetForecast[['ds', 'yhat']]
+          prophetForecast = prophetForecast.set_index('ds')
+          prophetForecast.reset_index(inplace=True)
+
+          #-----------bpnn-----------
+          dataset_bpnn = dataset.copy().tail(12)
+          dataset_bpnn['ds'] = pd.to_datetime(dataset_bpnn['ds'])
+          dataset_bpnn['hour'] = dataset_bpnn['ds'].dt.hour
+          dataset_bpnn['minute'] = dataset_bpnn['ds'].dt.minute
+          dataset_bpnn['day_of_week'] = dataset_bpnn['ds'].dt.dayofweek
+          
+          # drop ds and y column
+          dataset_bpnn = dataset_bpnn.drop(['ds', 'y'], axis=1)
+
+          #add lstm forecast to dataset
+          dataset_bpnn['lstm'] = lstmForecast
+          
+          # add prophet forecast to dataset
+          dataset_bpnn['prophet'] = prophetForecast['yhat'].values
+          
+          print("--------model loaded bpnn---------")
+          predictions = loaded_model_bpnn.predict(dataset_bpnn)
+        
+          # convert 2D to 1D array
+          predictions = predictions.flatten()
+
+          # round up to 2 decimal
+          arrayForecast = np.around(predictions, decimals=4)
+
+          # convert to list
+          listForecast = arrayForecast.tolist()
+
+          # convert to json
+          objectFormat['forecast'] = listForecast
+
+          return jsonify(objectFormat)
+
+        else:
+          print(f"File not found: {temp_lstm_weight} or {temp_bpnn_weight}")
+      except Exception as e:
+        print(e)
+  
   #-------------------Prophet-------------------
   def predictTempProphet():
     if request.method == 'POST':
